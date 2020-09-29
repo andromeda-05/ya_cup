669917b966039e14b3648fa1d5f2e4fb285e8b1f
andromeda-05 <andromeda-05@yandex.ru>
2020-09-29 10:10:28 +0400

    2 задача Старт

---
+++ 2_alarms.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+ 
+using std::string;
+using std::cin;
+using std::cout;
+ 
+int main() {
+    int N,X,K;
+    cin >> N >> X >> K;
+    
+ 
+    int result = 0;
+    for (const char ch : s) {
+        result += j.find(ch) != string::npos;
+    }
+ 
+    cout << result;
+ 
+    return 0;
+}
